9c5187504f0c3aac0faffccf3a80a0e6c89a0cdf
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-03-23 19:48:09 +0530

    remove unnecessary files

--- src/konnekt/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class SkillSearchForm(forms.Form):
-    search_text = forms.CharField(
-        required=False,
-        label='Search name or skill!',
-        widget=forms.TextInput(attrs={'placeholder': 'Search name or skill...'})
-    )
--- src/konnekt/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import HomeView, SearchView
-
-app_name = 'konnekt'
-
-urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='index'),
-    url(r'^search/$', SearchView.as_view(), name='search'),
-]
