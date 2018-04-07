1db2cc65c323f773a0f1979608d977c8db3c0288
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-04-08 04:27:06 +0530

    change database for production settings

--- src/gymkhana/settings/prod.py
+++ src/gymkhana/settings/prod.py
@@ -15,8 +15,12 @@ ALLOWED_HOSTS = ['students.iitj.ac.in']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gymkhanadb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
