ab1d5c06f0a06bcf263bd9209b05ae52d4872bf3
ajatprabha <ajat.prabha.leo@gmail.com>
2018-08-06 23:56:19 +0530

    add env config

--- src/gymkhana/wsgi.py
+++ src/gymkhana/wsgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 import sys
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-path = '/home/students/gymkhana/gymkhana/src'
+path = config('APP_SRC', cast=str)
 if path not in sys.path:
     sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymkhana.settings")
