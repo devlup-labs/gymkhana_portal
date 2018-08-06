61075f4fb044d3947ef5eb49cb6e861efeb10007
ajatprabha <ajat.prabha.leo@gmail.com>
2018-08-06 23:50:08 +0530

    update wsgi.py

--- src/gymkhana/wsgi.py
+++ src/gymkhana/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymkhana.settings.prod")
+path = '/home/students/gymkhana/gymkhana/src'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymkhana.settings")
 
 application = get_wsgi_application()
