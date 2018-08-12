15cf17d66d10ba2a03db8d1854a4f0ee0a91dba1
ajatprabha <ajat.prabha.leo@gmail.com>
2018-08-12 14:48:43 +0530

    add email guard

--- src/gymkhana/settings.py
+++ src/gymkhana/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'htmlmin.middleware.HtmlMinifyMiddleware',
     'htmlmin.middleware.MarkRequestMiddleware',
     'oauth.middleware.UserProfileExistsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware'
 ]
 
 SITE_ID = 1
@@ -140,6 +141,10 @@ LOGIN_REDIRECT_URL = 'forum:index'
 
 LOGIN_URL = 'login'
 
+LOGIN_ERROR_URL = '/login/'
+
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/login/'
+
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
@@ -152,6 +157,8 @@ SOCIAL_AUTH_CLEAN_USERNAMES = True
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_WHITELISTED_DOMAINS = ['iitj.ac.in']
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {'hd': 'iitj.ac.in'}
+
 SOCIAL_AUTH_PROTECTED_USER_FIELDS = ['username', 'email']
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY', default='', cast=str)
@@ -161,6 +168,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET', default='', ca
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
