4cd4904e4d042bc356ff8a8a3d0dbfc56a20e0f0
Anshul Ahuja <anshul.ahu@gmail.com>
2018-08-12 14:56:48 +0530

    removed registration success page

--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from .views import (ProfileDetailView, ProfileEditView, SocialLinkCreateView, SocialLinkUpdateView,
-                    SocialLinkDeleteView, RegisterView, RegisterSuccessView, AccountActivationView, get_activation_link)
+                    SocialLinkDeleteView, RegisterView, AccountActivationView, get_activation_link)
 
 app_name = 'oauth'
 
 urlpatterns = [
     url(r'^register/$', RegisterView.as_view(), name='register'),
-    url(r'^register-success/$', RegisterSuccessView.as_view(), name='register-success'),
     url(r'^(?P<roll>[\w]+)/get/$', get_activation_link, name='get-act-link'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         AccountActivationView.as_view(), name='activate'),
