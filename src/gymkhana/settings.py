afa5cfb377715ed973deb572368ff3dc86da7629
ajatprabha <ajat.prabha.leo@gmail.com>
2018-09-07 00:38:09 +0530

    always use postgresql as primary db

--- src/gymkhana/settings.py
+++ src/gymkhana/settings.py
@@ -100,22 +100,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gymkhana.wsgi.application'
 
-if DEBUG:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+if config('SQLITE_DB', cast=bool, default=False):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': '',
         }
     }
 
