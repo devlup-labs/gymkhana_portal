327693ac616cc77886ed9d6606a4e29016ba017c
ajatprabha <ajat.prabha.leo@gmail.com>
2018-11-23 01:39:24 +0530

    add report_link field

--- src/main/models.py
+++ src/main/models.py
@@ -47,6 +47,7 @@ class Society(models.Model):
     mentor = models.ForeignKey(UserProfile, related_name='smentor', limit_choices_to={'user__is_staff': True},
                                null=True, blank=True, on_delete=models.SET_NULL, default=None)
     faculty_advisor = models.ForeignKey(FacultyAdvisor, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    report_link = models.URLField(help_text='Add a drive link to show on society page', null=True, blank=True)
     gallery = models.ForeignKey(Gallery, blank=True, null=True, on_delete=models.SET_NULL,
                                 help_text="Select a carousel gallery to link to this society.")
     custom_html = models.TextField(blank=True, null=True, default=None,
@@ -166,6 +167,7 @@ class Senate(models.Model):
     members = models.ManyToManyField(UserProfile, through='SenateMembership', through_fields=('senate', 'userprofile'))
     coordinator_student = models.ForeignKey(FacultyAdvisor, blank=True, null=True, default=None,
                                             on_delete=models.SET_NULL)
+    report_link = models.URLField(help_text='Add a drive link to show on senate page', null=True, blank=True)
     custom_html = models.TextField(blank=True, null=True, default=None,
                                    help_text="Add custom HTML to view on society page.")
     slug = models.SlugField(unique=True, help_text="This will be used as URL. /senate/slug")
