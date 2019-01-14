1edb7f255de01145e7e700a8a6ca9c34e1474981
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-01-15 01:50:05 +0530

    Updated festival event models name

--- src/festivals/views.py
+++ src/festivals/views.py
@@ -1,13 +1,15 @@
 from django.views.generic import TemplateView
-from .models import EventCategory
+from .models import FestivalEventCategory
 
 
 class NimbleView(TemplateView):
     template_name = 'festivals/nimble/index.html'
-    model = EventCategory
+    model = FestivalEventCategory
 
     def get_context_data(self, **kwargs):
         context = super(NimbleView, self).get_context_data(**kwargs)
-        event_category_list = EventCategory.objects.filter(festival__name='NMBL', event__published=True).distinct()
+        event_category_list = FestivalEventCategory.objects.filter(festival__name='NMBL',
+                                                                   festivalevent__published=True).distinct()
+        print(event_category_list)
         context['event_category_list'] = event_category_list
         return context
