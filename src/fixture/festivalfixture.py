1369393c4ebb5ddcdfaee4171623729860827137
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-01-16 01:07:03 +0530

    Add fixture for festival events

--- src/fixture/festivalfixture.py
+++ src/fixture/festivalfixture.py
@@ -10,7 +10,36 @@ class FestivalFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'festivals.Festival'
 
-    name = random.choice(['IGNS', 'VRCHS', 'SPNDN', 'NMBL'])
+    name = random.choice(['Ignus', 'Verchas', 'Spandan', 'Nimble'])
     photo = factory.django.ImageField(color=random.choice(COLOUR))
     about = factory.Faker('sentence', nb_words=30)
+    slug = factory.Sequence(lambda n: 'fest-%d' % n)
     link = factory.Faker('url')
+    published = random.choice([True, False])
+    default_html = random.choice([True, False])
+
+
+class EventCategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'festivals.EventCategory'
+
+    name = factory.Faker('sentence', nb_words=3)
+    festival = factory.SubFactory(FestivalFactory)
+    cover = factory.django.ImageField(color=random.choice(COLOUR))
+    slug = factory.Sequence(lambda n: 'category-%d' % n)
+    about = factory.Faker('sentence', nb_words=10)
+
+
+class EventFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'festivals.Event'
+
+    event_category = factory.SubFactory(EventCategoryFactory)
+    name = factory.Faker('sentence', nb_words=3)
+    slug = factory.Sequence(lambda n: 'event-%d' % n)
+    unique_id = factory.Sequence(lambda n: 'event-%d' % n)
+    # description = factory.Faker('sentence', nb_words=30)
+    cover = factory.django.ImageField(color=random.choice(COLOUR))
+    max_team_size = 1
+    min_team_size = 1
+    published = True
