96847c9930026be0ed325b718898be73aaf35b4e
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-01-16 16:38:47 +0530

    Remove festival from context of FestivalView

--- src/festivals/views.py
+++ src/festivals/views.py
@@ -9,6 +9,5 @@ class FestivalView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(FestivalView, self).get_context_data(**kwargs)
         event_category_list = self.object.eventcategory_set.all().filter(event__published=True).distinct()
-        context['festival'] = self.object
         context['event_category_list'] = event_category_list
         return context
