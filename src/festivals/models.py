ee09bf137825e5b9adcf77f3418daa220f7fbed7
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-01-18 12:30:01 +0530

    add social link in festival app

--- src/festivals/models.py
+++ src/festivals/models.py
@@ -56,3 +56,54 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class SocialLink(models.Model):
+    SM_CHOICES = (
+        ('FB', 'Facebook'),
+        ('TW', 'Twitter'),
+        ('LI', 'LinkedIn'),
+        ('GP', 'Google Plus'),
+        ('IG', 'Instagram'),
+        ('GH', 'GitHub'),
+        ('YT', 'YouTube'),
+    )
+    FA_CHOICES = (
+        ('fa fa-facebook', 'FB'),
+        ('fa fa-twitter', 'TW'),
+        ('fa fa-linkedin', 'LI'),
+        ('fa fa-google-plus', 'GP'),
+        ('fa fa-instagram', 'IG'),
+        ('fa fa-github', 'GH'),
+        ('fa fa-youtube', 'YT'),
+    )
+    IC_CHOICES = (
+        ('fb-ic', 'FB'),
+        ('tw-ic', 'TW'),
+        ('li-ic', 'LI'),
+        ('gplus-ic', 'GP'),
+        ('ins-ic', 'IG'),
+        ('git-ic', 'GH'),
+        ('yt-ic', 'YT'),
+    )
+    festival = models.ForeignKey(Festival, on_delete=models.CASCADE)
+    social_media = models.CharField(max_length=2, choices=SM_CHOICES)
+    link = models.URLField()
+
+    class Meta:
+        ordering = ['social_media']
+
+    def __str__(self):
+        return self.festival.name + ' - ' + self.get_social_media_display()
+
+    def get_fai(self):
+        for key, value in self.FA_CHOICES:
+            if value == self.social_media:
+                return key
+        return 'fa fa-link'
+
+    def get_sm_ic(self):
+        for key, value in self.IC_CHOICES:
+            if value == self.social_media:
+                return key
+        return ''
