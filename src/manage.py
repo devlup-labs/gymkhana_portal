8965255a82dc42da45b4afa9b5e1204bd964e01e
hirenchalodiya1 <chalodiya.1@iitj.ac.in>
2019-12-27 22:25:34 +0530

    Update: branch coverage in manage.py
    - remove branch coverage from `if __name__ == "__main__"` line
    - remove additional pragmas from manage.py file
    - add new line at end of `.coveragerc` file

--- src/manage.py
+++ src/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no branch
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymkhana.settings")
     try:
         from django.core.management import execute_from_command_line
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
-        try:  # pragma: no cover
+        try:
             import django
-        except ImportError:  # pragma: no cover
+        except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
-        raise  # pragma: no cover
+        raise
     execute_from_command_line(sys.argv)
