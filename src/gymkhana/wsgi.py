41d38f696a795598dd5b81ebeeef8cc283d00505
ajatprabha <ajat.prabha.leo@gmail.com>
2020-01-30 02:07:52 +0530

    add docker-compose.yml for development purposes

--- src/gymkhana/wsgi.py
+++ src/gymkhana/wsgi.py
@@ -8,14 +8,8 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-import sys
-from decouple import config
-
 from django.core.wsgi import get_wsgi_application
 
-path = config('APP_SRC', cast=str)
-if path not in sys.path:
-    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymkhana.settings")
 
 application = get_wsgi_application()
