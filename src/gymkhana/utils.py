7209d0bd3486c0d591ebfdb54d09207ae62b951c
killbotXD <sonawane.1@iitj.ac.in>
2020-03-18 18:53:57 +0530

    remove redundant code

--- src/gymkhana/utils.py
+++ src/gymkhana/utils.py
@@ -6,7 +6,6 @@ from main.schema import RenditionType
 
 def build_image_types(request, image, key_set):
     data = []
-    for key, value in build_versatileimagefield_url_set(image, validate_versatileimagefield_sizekey_list(
-            get_rendition_key_set(key_set)), request=request).items():
+    for key, value in build_versatileimagefield_url_set(image, get_rendition_key_set(key_set), request=request).items():
         data.append(RenditionType(name=key, url=value))
     return data
