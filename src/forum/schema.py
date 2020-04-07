35c9b80f57a52415b54eeef5e77412d1d81d78b7
killbotXD <sonawane.1@iitj.ac.in>
2020-04-08 01:58:12 +0530

    Add create topic mutation

--- src/forum/schema.py
+++ src/forum/schema.py
@@ -1,13 +1,10 @@
-import graphene
-from graphene import relay, Field
+from graphene import relay
 from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.forms.mutation import DjangoModelFormMutation
+from graphql_jwt.decorators import login_required
 
 from forum.forms import TopicForm
 from forum.models import Topic, Answer
-from oauth.models import UserProfile
-from oauth.schema import UserProfileNode
 
 
 class AnswerNode(DjangoObjectType):
@@ -28,3 +25,17 @@ class TopicNode(DjangoObjectType):
     @classmethod
     def search(cls, query, indfo):
         return cls._meta.model.objects.search(query)
+
+
+class CreateTopicMutation(DjangoModelFormMutation):
+    class Meta:
+        form_class = TopicForm
+
+    @classmethod
+    @login_required
+    def perform_mutate(cls, form, info):
+        obj = form.save(commit=False)
+        obj.author = info.context.user.userprofile
+        obj.save()
+        kwargs = {cls._meta.return_field_name: obj}
+        return cls(errors=[], **kwargs)
